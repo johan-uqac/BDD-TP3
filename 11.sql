6922b1ade23de2f15f8455cff6c77850d0ea3678
Maxime Hoffbeck <maxime.hoffbeck@epitech.eu>
2024-03-30 18:38:00 -0400

    fix 11

--- 11.sql
+++ 11.sql
@@ -1,38 +1,30 @@
+
 CREATE TABLE IF NOT EXISTS archive_agents (
     id_agent INT PRIMARY KEY AUTO_INCREMENT,
     nom_agent VARCHAR(255),
     prenom_agent VARCHAR(255)
 );
 
-INSERT INTO archive_agents (id_agent, nom_agent, prenom_agent)
-SELECT 
-    id_agent, nom_agent, prenom_agent
-FROM 
-    agent_de_securite
-WHERE 
-    id_agent IN (
-        SELECT 
-            a.id_agent
-        FROM 
-            agent_de_securite a
-        LEFT JOIN 
-            espace_surveille es ON a.id_agent = es.id_agent
-        WHERE 
-            es.date_surveillance < DATE_SUB(NOW(), INTERVAL 12 MONTH)
-            OR es.date_surveillance IS NULL
-    );
-
-DELETE FROM agent_de_securite
-WHERE id_agent IN (
-    SELECT 
-        a.id_agent
-    FROM 
-        (SELECT id_agent FROM agent_de_securite) AS a
-    LEFT JOIN 
-        espace_surveille es ON a.id_agent = es.id_agent
-    WHERE 
-        es.date_surveillance < DATE_SUB(NOW(), INTERVAL 12 MONTH)
-        OR es.date_surveillance IS NULL
+SELECT id_agent, nom_agent, prenom_agent
+FROM agent_de_securite
+WHERE id_agent NOT IN (
+    SELECT DISTINCT id_agent
+    FROM espace_surveille
+    WHERE date_surveillance >= DATE_SUB(NOW(), INTERVAL 12 MONTH)
 );
 
--- A REVERIFIER CE SCRIPT
+INSERT INTO archive_agents (nom_agent, prenom_agent)
+SELECT nom_agent, prenom_agent
+FROM agent_de_securite
+WHERE id_agent NOT IN (
+    SELECT DISTINCT id_agent
+    FROM espace_surveille
+    WHERE date_surveillance >= DATE_SUB(NOW(), INTERVAL 12 MONTH)
+);
+
+DELETE FROM agent_de_securite
+WHERE id_agent NOT IN (
+    SELECT DISTINCT id_agent
+    FROM espace_surveille
+    WHERE date_surveillance >= DATE_SUB(NOW(), INTERVAL 12 MONTH)
+);
